99d743cda34bae5a8c6345ec645a423544480a1b
Tomasz Krutowski <tkrutowski@gmail.com>
2021-02-07 14:26:07 +0100

    init

---
+++ src/main/java/net/focik/taskcalendar/TaskCalendarApplication.java
@@ -0,0 +1,13 @@
+package net.focik.taskcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCalendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCalendarApplication.class, args);
+	}
+
+}
