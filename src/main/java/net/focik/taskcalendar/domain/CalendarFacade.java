fa341d4e4d8ed7d00a8b654580e4f21aacc143b6
Tomasz Krutowski <tkrutowski@gmail.com>
2022-03-11 10:55:46 +0100

    refactor change message use case

--- src/main/java/net/focik/taskcalendar/domain/CalendarFacade.java
+++ src/main/java/net/focik/taskcalendar/domain/CalendarFacade.java
@@ -1,27 +1,23 @@
 package net.focik.taskcalendar.domain;
 
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import net.focik.taskcalendar.domain.port.secondary.ICalendarEntryRepository;
-import net.focik.taskcalendar.domain.share.MailStatus;
-import net.focik.taskcalendar.infrastructure.dto.EntryDbDto;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CalendarFacade {
 
-    private final EntryDtoFactory entryDtoFactory;
+    private final TaskCalendarService calendarService;
 
     public List<ICalendarEntry> getCalendarEntriesByDate(LocalDate startDate, int howManyDays ) {
-        int i=0;
-        return entryDtoFactory.createCalendarEntries(startDate, howManyDays);
-
+        return  calendarService.getCalendarEntriesByDate(startDate, howManyDays);
     }
 
 
+    public boolean changeInfoMessage(Integer idEntry, String msg) {
+        return calendarService.saveInfoMsg(idEntry,msg);
+    }
 }
