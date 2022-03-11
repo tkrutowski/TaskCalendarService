236d5debf07dc9282d36afc499a2bbaa7dab57dd
Tomasz Krutowski <tkrutowski@gmail.com>
2022-03-11 11:30:15 +0100

    update CalendarEntries

--- src/main/java/net/focik/taskcalendar/domain/GasMainEntry.java
+++ src/main/java/net/focik/taskcalendar/domain/GasMainEntry.java
@@ -2,17 +2,13 @@ package net.focik.taskcalendar.domain;
 
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 import net.focik.taskcalendar.domain.share.MailStatus;
 import net.focik.taskcalendar.domain.share.TaskType;
 
 import java.time.LocalDate;
 
-@Setter
-@Getter
 @Builder(access = AccessLevel.PACKAGE)
-class GasMainEntry implements ICalendarEntry{
+public class GasMainEntry implements ICalendarEntry {
     private Integer idEntry;
     private Integer idTask;
     private Integer idTeam;
@@ -26,4 +22,115 @@ class GasMainEntry implements ICalendarEntry{
     private Integer idSurveyor;
     private String address;
     private String taskNo;
+
+    @Override
+    public void changeMessage(String msg) {
+        message = msg;
+    }
+
+    @Override
+    public Integer getIdEntry() {
+        return idEntry;
+    }
+
+    @Override
+    public Integer getIdTask() {
+        return idTask;
+    }
+
+    @Override
+    public Integer getIdTeam() {
+        return idTeam;
+    }
+
+    @Override
+    public TaskType getTaskType() {
+        return taskType;
+    }
+
+    @Override
+    public LocalDate getDate() {
+        return date;
+    }
+
+    @Override
+    public MailStatus getSentMailPgnStatus() {
+        return MailStatus.NO_NEED;
+    }
+
+    @Override
+    public LocalDate getPostDatePgn() {
+        return null;
+    }
+
+    @Override
+    public MailStatus getSentMailToSurveyorStatus() {
+        return sentMailToSurveyor;
+    }
+
+    @Override
+    public LocalDate getPostDateSurveyor() {
+        return dateSentMailToSurveyor;
+    }
+
+    @Override
+    public MailStatus getSentMailToCustomerStatus() {
+        return MailStatus.NO_NEED;
+    }
+
+    @Override
+    public LocalDate getPostDateCustomer() {
+        return null;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public Integer getIdSurveyor() {
+        return idSurveyor;
+    }
+
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public String getTaskNo() {
+        return null;
+    }
+
+    @Override
+    public void setSentMailToCustomerStatus(MailStatus mailStatus) {
+        //in the future
+    }
+
+    @Override
+    public void setPostDateCustomer(LocalDate date) {
+        //in the future
+    }
+
+    @Override
+    public void setSentMailToSurveyorStatus(MailStatus mailStatus) {
+        this.sentMailToSurveyor = mailStatus;
+    }
+
+    @Override
+    public void setPostDateSurveyor(LocalDate date) {
+        this.dateSentMailToSurveyor = date;
+    }
+
+    @Override
+    public void setSentMailPgnStatus(MailStatus mailStatus) {
+        //in the future
+    }
+
+    @Override
+    public void setPostDatePgn(LocalDate date) {
+        //in the future
+    }
 }
--- src/main/java/net/focik/taskcalendar/domain/ICalendarEntry.java
+++ src/main/java/net/focik/taskcalendar/domain/ICalendarEntry.java
@@ -1,8 +1,44 @@
 package net.focik.taskcalendar.domain;
 
+import net.focik.taskcalendar.domain.share.MailStatus;
+import net.focik.taskcalendar.domain.share.TaskType;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public interface ICalendarEntry {
+    void changeMessage(String msg);
+
+    Integer getIdEntry();
+    Integer getIdTask();
+    Integer getIdTeam();
+    TaskType getTaskType();
+    LocalDate getDate();
+
+    MailStatus getSentMailPgnStatus();
+    LocalDate getPostDatePgn();
+
+    MailStatus getSentMailToSurveyorStatus();
+    LocalDate getPostDateSurveyor();
+
+    MailStatus getSentMailToCustomerStatus();
+    LocalDate getPostDateCustomer();
+
+    String getMessage();
+    Integer getIdSurveyor();
+    String getAddress();
+    String getTaskNo();
+
+
+    void setSentMailToCustomerStatus(MailStatus mailStatus);
+    void setPostDateCustomer(LocalDate date);
+
+    void setSentMailToSurveyorStatus(MailStatus mailStatus);
+    void setPostDateSurveyor(LocalDate date);
+
+    void setSentMailPgnStatus(MailStatus mailStatus);
+    void setPostDatePgn(LocalDate date);
+
 
 }
